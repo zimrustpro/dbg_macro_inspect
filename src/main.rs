b3f0aa9ccadb9fe7bd842f024f436796c2e7385d
zimrustpro <zimrustpro@gmail.com>
2024-11-22 21:39:59 +0200

    Longer closure inside closure

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,14 @@ fn main() {
     let new_vec = vec![8, 9, 10];
     let _d_vec = new_vec
         .iter()
-        .inspect(|first_item| println!("The item is: {}", first_item))
+        .inspect(|item| {
+            println!("The item is: {}", item);
+            match **item % 2 {
+                0 => println!("It is even."),
+                _ => println!("It is odd."),
+            }
+            println!("In binary it is {:b}.", item);
+        })
         .map(|x| x * 2)
         .inspect(|next_item| println!("Then it is: {}", next_item))
         .collect::<Vec<i32>>();
