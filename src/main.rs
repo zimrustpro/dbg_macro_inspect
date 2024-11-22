78069f2ba007f053844c7ba1c6408630e4f9288b
zimrustpro <zimrustpro@gmail.com>
2024-11-22 21:13:47 +0200

    Use `dbg!` macro to print info about lines of code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let my_number = 8;
+    dbg!(my_number);
+
+    let mut _my_number = 9;
+    dbg!(_my_number += 10);
+    let new_vec = dbg!(vec![8, 9, 10]);
+    let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    dbg!(double_vec);
 }
