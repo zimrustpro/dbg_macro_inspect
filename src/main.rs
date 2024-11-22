55a489c58fc3fdc1f7a0ac99fafdd3bfc431e8a6
zimrustpro <zimrustpro@gmail.com>
2024-11-22 21:20:43 +0200

    Use `inspect()` to look at iterator operations

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,12 @@ fn main() {
     let new_vec = dbg!(vec![8, 9, 10]);
     let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
     dbg!(double_vec);
+
+    let new_vec = vec![8, 9, 10];
+    let _d_vec = new_vec
+        .iter()
+        .inspect(|first_item| println!("The item is: {}", first_item))
+        .map(|x| x * 2)
+        .inspect(|next_item| println!("Then it is: {}", next_item))
+        .collect::<Vec<i32>>();
 }
